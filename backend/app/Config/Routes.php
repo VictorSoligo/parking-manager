4a27329fa8d24ad081274ec11baea16e879925f7
Victor Hugo Soligo <victor@pop-os.localdomain>
2024-06-11 09:47:41 -0300

    backend dev container setup

--- backend/app/Config/Routes.php
+++ backend/app/Config/Routes.php
@@ -6,6 +6,3 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
-
-$routes->get('heroes/(:num)', 'HeroController::show/$1');
-$routes->get('dungeons/(:num)', 'DungeonController::show/$1');
