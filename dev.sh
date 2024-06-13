523ac89637d4f9c13eccd72b8b7ad12d946f9bc7
Victor Hugo Soligo <victor@pop-os.localdomain>
2024-06-13 08:51:20 -0300

    dev script

---
+++ dev.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# chmod +x dev.sh
+
+(cd frontend && npm i)
+(cd backend && composer install)
+
+tmux new-session -d -s frontend 'cd frontend && npm run dev'
+tmux new-session -d -s backend 'cd backend && php spark serve --host 0.0.0.0'
+
+clear
+
+echo "🚀 Backend running at http://localhost:8080"
+echo "🚀 Frontend running at http://localhost:3000"
