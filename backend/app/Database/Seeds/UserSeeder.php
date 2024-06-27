22ede3219d6bb4a5d9f7975ca0168582e5f53ffe
Victor Hugo Soligo <54515535+VictorSoligo@users.noreply.github.com>
2024-06-27 15:33:29 -0300

    chore: update seed user

--- backend/app/Database/Seeds/UserSeeder.php
+++ backend/app/Database/Seeds/UserSeeder.php
@@ -9,9 +9,9 @@ class UserSeeder extends Seeder
     public function run()
     {
         $data = [
-            'name' => 'Vitao',
+            'name' => 'Admin Master',
             'role' => 'admin',
-            'email' => 'vitao@gmail.com',
+            'email' => 'admin@admin.com',
             'password' => '$2y$10$z0TlOr.kEtzVlDjQhyxlUOQqWsUAZR4/845lyi4/Vak46FauYs5VK',
             'created_at' => '2024-06-20 22:51:38',
             'updated_at' => '2024-06-20 22:51:38',
